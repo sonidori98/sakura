60d5a3ba33cdff9166c84c845b6b6e3afb867d22
Green-EyedMonster <G-Free_159@proton.me>
2024-09-03 19:17:39 +0900

    update: ターミナルのサイズを取得

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
 use console::{Style, Term};
+use crossterm::terminal::size;
 use rand::Rng;
+use core::panic;
 use std::{
-    sync::{Arc, Mutex},
+    sync::{Arc, Mutex, OnceLock},
     thread::sleep,
     time::{Duration, Instant},
 };
 
-const WIDTH: i32 = 100;
-const HEIGHT: i32 = 30;
+// const WIDTH: i32 = 100;
+// const HEIGHT: i32 = 30;
+static WIDTH: OnceLock<i32> = OnceLock::new();
+static HEIGHT: OnceLock<i32> = OnceLock::new();
 const PETAL_COUNT: usize = 25;
 const PETAL_GENERATION_INTERVAL_MS: u64 = 350;
+pub const ONCE_ERR: &str = "Value should be set";
 
 fn main() {
     let term = Arc::new(Mutex::new(Term::stdout()));
     let pink = Style::new().color256(225);
     let brown = Style::new().color256(136);
+    get_size();
 
     let petal = "*";
 
@@ -40,7 +46,7 @@ fn main() {
     let foliage_height = foliage.lines().count() as i32;
     let trunk_height = trunk.lines().count() as i32;
 
-    let y = HEIGHT - trunk_height - foliage_height;
+    let y = *HEIGHT.get().expect(ONCE_ERR) - trunk_height - foliage_height;
 
     let mut rng = rand::thread_rng();
 
@@ -76,7 +82,7 @@ fn main() {
 
     loop {
         for &(px, py) in &petals {
-            if (0..WIDTH).contains(&px) && (0..HEIGHT).contains(&py) {
+            if (0..*WIDTH.get().expect(ONCE_ERR)).contains(&px) && (0..*HEIGHT.get().expect(ONCE_ERR)).contains(&py) {
                 term.lock()
                     .unwrap()
                     .move_cursor_to(px as usize, py as usize)
@@ -85,13 +91,13 @@ fn main() {
             }
         }
 
-        petals.retain(|&(_px, py)| py < HEIGHT);
+        petals.retain(|&(_px, py)| py < *HEIGHT.get().expect(ONCE_ERR));
 
         for petal in petals.iter_mut() {
             petal.1 += 1;
             petal.0 += rng.gen_range(1..=10);
 
-            if petal.1 >= HEIGHT {
+            if petal.1 >= *HEIGHT.get().expect(ONCE_ERR) {
                 petal.0 = petal_start_x;
                 petal.1 = petal_start_y;
             }
@@ -109,7 +115,7 @@ fn main() {
         }
 
         for &(px, py) in &petals {
-            if (0..WIDTH).contains(&px) && (0..HEIGHT).contains(&py) {
+            if (0..*WIDTH.get().expect(ONCE_ERR)).contains(&px) && (0..*HEIGHT.get().expect(ONCE_ERR)).contains(&py) {
                 term.lock()
                     .unwrap()
                     .move_cursor_to(px as usize, py as usize)
@@ -121,5 +127,18 @@ fn main() {
         term.lock().unwrap().flush().unwrap();
 
         sleep(Duration::from_millis(200));
+    }
+}
+
+fn get_size() {
+    match size() {
+        Ok((col, row)) => {
+            HEIGHT.set(col.into()).unwrap();
+            WIDTH.set(row.into()).unwrap();
+        }
+        Err(e) => {
+            eprintln!("Failed to get terminal size: {}", e);
+            panic!();
+        }
     }
 }
